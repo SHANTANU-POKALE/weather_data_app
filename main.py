bd3b0854fd853e018b6935dc862b22e8b0bf95a9
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-07-05 12:59:08 +0530

    day32; plot fajke data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forcast for the Next Days")
 place = st.text_input("place: ")
@@ -9,3 +10,13 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 
+def get_data(days):
+    dates = ["2024-25-06", "2024-26-06", "2024-27-06", "2024-28-06"]
+    temperatures = [10, 11, 15, 19]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (c)"})
+st.plotly_chart(figure)
