df7ff45646fe193044585e598badf4cbcf662d81
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-07-07 21:00:22 +0530

    day32: added the sky and temp to main

--- backend.py
+++ backend.py
@@ -2,22 +2,18 @@ import requests
 
 API_KEY = "41f2442a71676225124d775da7185277"
 
-def get_data(place, forcast_days, kind):
+def get_data(place, forcast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forcast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Amravati", forcast_days=3, kind="Temperature"))
+    print(get_data(place="Amravati", forcast_days=3))
 
 
 
